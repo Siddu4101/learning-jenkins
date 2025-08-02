c11d97ad257dd8ea4a84a7b5f7f43d7bcb89162f
Siddu4101 <siddu412001@gmail.com>
2025-08-02 19:25:04 +0530

    pull the code from git and run the code if failure or success send the mail

---
+++ src/main/java/org/example/jenkins/GitHubPullAndRunJob.sh
@@ -0,0 +1,3 @@
+echo "passing only the file path not mount path as it picks from the workspace of this container where it is running"
+echo "and the cloned project will be directly present in the workspace of the container"
+bash src/main/java/org/example/jenkins/HelloWorld.sh
